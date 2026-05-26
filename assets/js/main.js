549f02564eb21f710a5a519c2d71a73d4f8e9705
andy-and-terry <yucheng.lee29@gmail.com>
2026-05-27 07:11:28 +0800

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+const audio = new Audio('../audio/bg.mp3');
+audio.loop = true;
+audio.play();
+
+function onButton1() {
+  console.log('Button 1 pressed');
+}
+
+function onButton2() {
+  console.log('Button 2 pressed');
+}
+
+function onButton3() {
+  console.log('Button 3 pressed');
+}
